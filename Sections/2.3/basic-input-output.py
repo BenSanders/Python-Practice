fb55c8fea1128144b1b7c8693e2d6ff24241f0e0
Ben Sanders <ben@sanderstg.com>
2022-12-14 11:49:42 -0500

    Some more coursework

---
+++ Sections/2.3/basic-input-output.py
@@ -0,0 +1,49 @@
+print('Hello world!')
+
+# Each print statement starts with a new line
+print('Hello there.')
+print('My name is...')
+print('Carl?')
+
+print('3 2 1 Go!')
+
+print('*****')
+print('*   *')
+print('*****')
+print('*   *')
+print('*****')
+
+# including end=' ' keeps output on the same line
+print('Hello there.', end=' ')
+print('My name is...', end=' ')
+print('Carl?')
+
+# Printing the value of a variable
+wage = 20
+
+print('Wage is', end=' ')
+print(wage) # print variable's value
+print('Goodbye.')
+
+# Printing multiple items using a single print statement
+wage = 20
+
+print('Wage:', wage) # Comma separates multiple items
+print('Goodbye.')
+
+# Newline character
+print('1\n2\n3\n')
+
+# Printing without text
+print('123')
+print()
+print('abc')
+
+country_population = 1344130000
+country_name = "China"
+
+print('The population of', end=' ')
+print(country_name, end=' ')
+print('was', end=' ')
+print(country_population, end=' ')
+print('in 2011')
