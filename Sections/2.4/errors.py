654fe6c8a8bf87471d615355dd4d8149d3e4e99b
Ben Sanders <ben@sanderstg.com>
2022-12-20 07:09:53 -0500

    Added more to errors.py

--- Sections/2.4/errors.py
+++ Sections/2.4/errors.py
@@ -8,3 +8,27 @@ print('Woof!")
 
 print(Woof!) # Needs quotes
 print("Hello + friend!") #Runs perfectly fine
+
+triangle_base = 0 # Triangle base (cm)
+triangle_height = 0 #Triangle height (cm)
+triangle_area = 0 # Triangle area (cm)
+
+print("Enter triangle base(cm):")
+triangle_base = int(input())
+
+print("Enter triangle height(cm):")
+triangle_height = int(input())
+
+# Calculate triangle area
+
+triangle_area = (triangle_base*triangle_height)/2 # Print out the triangle base, height, and area
+
+print('Triangle_area=(',end=''))
+print(triangle_base)
+
+print('*',end='')
+
+print(triangle_height, end='')
+print(')/2=',end='')
+print(triangle_area,end='')
+print('cm**2')
