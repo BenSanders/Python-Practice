f5e708cad271c69347c8df0b4819282bcb78facd
Ben Sanders <ben@sanderstg.com>
2022-12-20 10:48:21 -0500

    Finished section 2.5

--- Sections/2.4/errors.py
+++ Sections/2.4/errors.py
@@ -32,3 +32,25 @@ print(triangle_height, end='')
 print(')/2=',end='')
 print(triangle_area,end='')
 print('cm**2')
+
+"""
+Error type 		|	Description
+SyntaxError		|	The program contains invalid code that cannot be understood
+IndentionError	|	The lines of the program are not properly indented
+ValueError		|	An invalid value is used - can occur if giving letters to in()
+NameError		|	The program tries to use a variable that does not exist
+TypeError		|	An operation uses incorrect types - can occur if adding an integer to a string
+"""
+
+# A logic error is often called a bug
+
+current_salary = int(input('Enter current salary:'))
+
+raise_percentage = 5  # Logic error gives a 500% raise instead of 5%.
+new_salary = current_salary + (current_salary * raise_percentage)
+print('New salary:', new_salary)
+
+print("Predictions are hard.")
+print("Especially about the future.")
+user_num = 5
+print('user_num is: ' user_num)
