eeefddd58de28aaaefae71fa63f5d70f40e88271
Ben Sanders <ben@sanders.life>
2023-01-08 19:53:06 -0500

    Added notes to 3.6

---
+++ Sections/3/3.6/average.py
@@ -0,0 +1,10 @@
+avg_sales = 0
+
+num_sales1 = int(input())
+num_sales2 = int(input())
+num_sales3 = int(input())
+
+''' Your solution goes here '''
+avg_sales = float(num_sales1 + num_sales2 + num_sales3) / 3
+
+print('Average sales: {:.2f}'.format(avg_sales))
---
+++ Sections/3/3.6/sphere.py
@@ -0,0 +1,9 @@
+pi = 3.14159
+sphere_volume = 0.0
+
+sphere_radius = float(input())
+
+''' Your solution goes here '''
+
+
+print('Sphere volume: {:.2f}'.format(sphere_volume))
