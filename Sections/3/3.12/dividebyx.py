bff5e4719fa5d8a868183a8dc5fa1e37856319dd
Ben Sanders <ben@sanders.life>
2023-01-17 03:48:50 -0500

    Added Sections 3.10, 3.11, 3.12

---
+++ Sections/3/3.12/dividebyx.py
@@ -0,0 +1,10 @@
+# user_num and x as input
+# 2000
+# 2
+user_num = int(input())
+x = int(input())
+
+#output user_num // x three times
+print(user_num // x, user_num // x // x, user_num // x // x // x)
+
+# Passed in Zybooks
