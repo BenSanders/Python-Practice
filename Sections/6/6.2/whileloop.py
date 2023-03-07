c68c7ca9f5e7e0cf8c163df3dacc11562907da71
Ben Sanders <ben@sanders.life>
2023-03-07 06:03:23 -0500

    Added comments to 6.2 lab

--- Sections/6/6.2/whileloop.py
+++ Sections/6/6.2/whileloop.py
@@ -1,3 +1,13 @@
+#  Write a while loop that repeats while user_num ≥ 1. In each loop iteration, first divide user_num by 2, then print user_num.
+# 
+# Sample output with input: 20
+# 
+# 10.0
+# 5.0
+# 2.5
+# 1.25
+# 0.625
+
 user_num = int(input())
 i = 1
 
